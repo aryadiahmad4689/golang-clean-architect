ab4ee82318062582ca48f6fe0c4cddd9edba7a42
padaelocomunity2 <sahrulalfa45@gmail.com>
2021-07-20 19:45:43 +0800

    test lagi

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	database "golang-clean-architecture/Database"
 	route "golang-clean-architecture/Route"
 	"net/http"
 
@@ -8,6 +10,12 @@ import (
 )
 
 func main() {
+	// migrate := flag.Bool("migrate", false, "to migrate table")
+	// if *migrate {
+	// 	MigrateTable()
+	// 	return
+	// }
+	MigrateTable()
 	mux := route.Route()
 	server := http.Server{
 		Addr:    "localhost:8080",
@@ -18,4 +26,13 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func MigrateTable() {
+	db, _ := database.GetConnection()
+	defer db.Close()
+	ctx := context.Background()
+	res, _ := db.ExecContext(ctx, "CREATE table IF NOT EXISTS user(id int PRIMARY key auto_increment,nama VARCHAR(100),alamat VARCHAR(100), age int, married boolean, date date)")
+	res.RowsAffected()
+	// return err
 }
